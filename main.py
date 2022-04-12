4ea0e4d2faf15f27426222086b287b3453877b16
Carlo <carlomonroy1997@gmail.com>
2022-04-11 22:23:22 -0500

    creting tables in postresql

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ class Comment(db.Model):
     parent_post = relationship("BlogPost", back_populates="comments")
 
 
-db.create_all()
+#db.create_all()
 
 #Define the user loader
 @login_manager.user_loader
